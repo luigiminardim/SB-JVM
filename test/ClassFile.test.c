a73d52036cbd621563c4687a5ccf229c5a107681
Luigi Minardi <luigiminardim@gmail.com>
2022-12-27 22:10:15 -0300

    feat: :sparkles: Add missing fields from ClassFile.

--- test/ClassFile.test.c
+++ test/ClassFile.test.c
@@ -11,6 +11,12 @@ TEST_CASE("ClassFile_read")
     REQUIRE(c.magic_number == 0xCAFEBABE);
     REQUIRE(c.minor_version == 0);
     REQUIRE(c.major_version == 52);
+    REQUIRE(c.constant_pool_count == 36);
+    // REQUIRE(c.access_flags == 0x0002); // ACC_PRIVATE // @todo test this
+    REQUIRE(c.this_class == 3);
+    REQUIRE(c.super_class == 8);
+    REQUIRE(c.interfaces_count == 0);
+    REQUIRE(c.fields_count == 1);
   }
   ClassFile_free(c);
   fclose(fp);
