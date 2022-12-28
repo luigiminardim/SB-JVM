82aa2c98597d558cb9edbb005ced36e692974dee
thiago <thiago.tokarski2001@gmail.com>
2022-12-28 19:38:22 -0300

    feat: :sparkles: Adicionar assinaturas e struct exceptiontable

--- src/AttributeInfo.h
+++ src/AttributeInfo.h
@@ -10,6 +10,18 @@ typedef struct ConstantValueAttribute
   u2 constantvalue_index;
 } ConstantValueAttribute;
 
+ConstantValueAttribute ConstantValueAttribute_read(FILE *fp);
+
+typedef struct ExceptionTable
+{
+  u2 start_pc;
+  u2 end_pc;
+  u2 handler_pc;
+  u2 catch_type;
+} ExceptionTable;
+
+ExceptionTable* ExceptionTable_read(FILE *fp, u2 exception_table_length);
+
 typedef struct CodeAttribute
 {
   u2 max_stack;
@@ -17,16 +29,20 @@ typedef struct CodeAttribute
   u4 code_length;
   u1 *code;
   u2 exception_table_length;
-  // ExceptionTable *exception_table;
+  ExceptionTable *exception_table;
   u2 attributes_count;
   AttributeInfo *attributes;
 } CodeAttribute;
+
+CodeAttribute CodeAttribute_read(FILE *fp, ConstantPool constant_pool);
 
 typedef struct ExceptionsAttribute
 {
   u2 number_of_exceptions;
   u2 *exception_index_table;
 } ExceptionsAttribute;
+
+ExceptionsAttribute ExceptionsAttribute_read(FILE *fp);
 
 // @todo Maybe implement this later
 // typedef struct InnerClassesAttribute
@@ -55,6 +71,8 @@ typedef struct AttributeInfo
 } AttributeInfo;
 
 AttributeInfo* AttributeInfo_read(FILE *fp, u2 attributes_count, ConstantPool constant_pool);
+
+void AttributeInfo_free(AttributeInfo* attributes, u2 attributes_count, ConstantPool constant_pool);
 
 char* AttributeInfo_to_string(AttributeInfo *attribute_info);
 
