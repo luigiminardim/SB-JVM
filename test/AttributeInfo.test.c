4836fa8bbfc0a9166b607ece046defe3609f780c
Toskosz <thiago.tokarski2001@gmail.com>
2022-12-30 01:53:33 -0300

    feat: :sparkles: Adicionar mais testes de atributos

--- test/AttributeInfo.test.c
+++ test/AttributeInfo.test.c
@@ -7,22 +7,32 @@ TEST_CASE("Attribute_read"){
     FILE *fp = fopen("assets/test-AttributeInfo/SumNumber.class", "rb");
     ClassFile c = ClassFile_read(fp);
     CpInfo cpinfo;
+    CpInfo tmpCPinfo;
 
     SECTION("CONSTANT_VALUE"){
         cpinfo = c.constant_pool[c.fields[1].attributes->constant_value.constantvalue_index];
         REQUIRE(cpinfo.constant_integer_info.bytes == 2);
     }
     
-    // Precisam de methodinfo implementado
-    //SECTION("CODE"){
-    //    
-    //}
+    SECTION("CODE"){
+        REQUIRE(c.methods[3].attributes[0].code.max_stack == 2);
+    }
     
     //SECTION("EXCEPTION"){
     //    
     //}
     
-    //SECTION("SOURCEFILE"){
-    //}
+    SECTION("EXCEPTION_Table"){
+        cpinfo = c.constant_pool[c.methods[2].attributes[0].code.exception_table[0].catch_type];
+        tmpCPinfo = c.constant_pool[cpinfo.constant_class_info.name_index];
+        char* test_catch_type = tmpCPinfo.constant_utf8_info.bytes;
+        REQUIRE(strcmp(test_catch_type, "java/lang/ArrayIndexOutOfBoundsException") == 0);
+    }
+
+    SECTION("SOURCEFILE"){
+        cpinfo = c.constant_pool[c.attributes[0].source_file.sourcefile_index];
+        char* test_source = cpinfo.constant_utf8_info.bytes;
+        REQUIRE(strcmp(test_source, "SumNumber.java") == 0);
+    }
 
 }
