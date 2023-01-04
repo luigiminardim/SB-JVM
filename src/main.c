8dc2520f8504d1bb4c3ae62d3c4303599e80a377
Luigi Minardi <luigiminardim@gmail.com>
2023-01-03 21:04:16 -0300

    fix: :bug: Corrigir leitura do argumento passado para o programa.

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
   char default_file_name[] = "assets/test-ConstantPool/AllConstanPoolCases.class";
-  char *file_name = argc > 2 ? argv[1] : default_file_name;
+  char *file_name = argc > 1 ? argv[1] : default_file_name;
   FILE *fp = fopen(file_name, "rb");
   ClassFile c = ClassFile_read(fp);
   char *s = ClassFile_to_string(c);
