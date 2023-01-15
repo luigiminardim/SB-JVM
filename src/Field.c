21d069161a684fd1aee2bda29596cc17d7f4eb90
Luigi Minardi <luigiminardim@gmail.com>
2023-01-14 23:19:31 -0300

    feat: :sparkles: Adicionar texto nos access_flags.

--- src/Field.c
+++ src/Field.c
@@ -3,6 +3,7 @@
 #include "java-bytes.h"
 #include <stdio.h>  // sprintf
 #include <stdlib.h> // malloc
+#include <string.h> // strlen
 
 FieldInfo *FieldInfo_read(FILE *fp, u2 fields_count, ConstantPool constant_pool)
 {
@@ -30,6 +31,31 @@ void FieldInfo_free(FieldInfo *field_infos, u2 fields_count, ConstantPool consta
   free(field_infos);
 }
 
+char *FieldInfo_access_flags_to_string(u2 access_flags)
+{
+  const char *public_string = access_flags & 0x0001 ? "public " : "";
+  const char *private_string = access_flags & 0x0002 ? "private " : "";
+  const char *protected_string = access_flags & 0x0004 ? "protected " : "";
+  const char *static_string = access_flags & 0x0008 ? "static " : "";
+  const char *final_string = access_flags & 0x0010 ? "final " : "";
+  const char *volatile_string = access_flags & 0x0040 ? "volatile " : "";
+  const char *transient_string = access_flags & 0x0080 ? "transient " : "";
+  const char *synthetic_string = access_flags & 0x1000 ? "synthetic " : "";
+  const char *enum_string = access_flags & 0x4000 ? "enum " : "";
+  char *str = (char *)malloc(1024 * sizeof(char));
+  snprintf(
+      str, 1024,
+      "%s%s%s%s%s%s%s%s%s",
+      public_string, private_string, protected_string, static_string,
+      final_string, volatile_string, transient_string, synthetic_string,
+      enum_string);
+  if (strlen(str) > 0)
+  {
+    str[strlen(str) - 1] = '\0'; // remove last space
+  }
+  return str;
+}
+
 char *FieldInfo_to_string(FieldInfo *field_infos, u2 fields_count, ConstantPool constant_pool)
 {
   char *str = (char *)malloc(65536 * sizeof(char));
@@ -41,6 +67,8 @@ char *FieldInfo_to_string(FieldInfo *field_infos, u2 fields_count, ConstantPool 
   snprintf(str, 65536, "[");
   for (FieldInfo *field_info = field_infos; field_info < field_infos + fields_count; field_info++)
   {
+    char *access_flags_str = FieldInfo_access_flags_to_string(
+        field_info->access_flags);
     char *attribute_info_str = AttributeInfo_to_string(
         field_info->attributes, field_info->attributes_count, constant_pool);
     char separator = field_info == field_infos + fields_count - 1 ? ']' : ',';
@@ -51,12 +79,13 @@ char *FieldInfo_to_string(FieldInfo *field_infos, u2 fields_count, ConstantPool 
         constant_pool, field_info->descriptor_index);
     snprintf(
         str_temp, 65536,
-        "%s{\"access_flags\": \"0x%X\",\"name_index\":\"#%d // %s\","
-        "\"descriptor_index\":\"#%d // %s\",\"attributes_count\":%d,"
-        "\"attributes\":%s}%c",
-        str, field_info->access_flags, field_info->name_index, name_utf8,
-        field_info->descriptor_index, descriptor_utf8, field_info->attributes_count,
-        attribute_info_str, separator);
+        "%s{\"access_flags\": \"0x%04X [%s]\",\"name_index\":\"#%d // %s\","
+        "\"descriptor_index\":\"#%d // %s\","
+        "\"attributes_count\":%d,\"attributes\":%s}%c",
+        str, field_info->access_flags, access_flags_str, field_info->name_index,
+        name_utf8, field_info->descriptor_index, descriptor_utf8,
+        field_info->attributes_count, attribute_info_str, separator);
+    free(access_flags_str);
     free(name_utf8);
     free(descriptor_utf8);
     free(attribute_info_str);
--- src/Method.c
+++ src/Method.c
@@ -3,6 +3,7 @@
 #include "java-bytes.h"
 #include <stdio.h>  // sprintf
 #include <stdlib.h> // malloc
+#include <string.h> // strlen
 
 MethodInfo *MethodInfo_read(FILE *fp, u2 methods_count, ConstantPool constant_pool)
 {
@@ -29,6 +30,34 @@ void MethodInfo_free(MethodInfo *method_info, u2 methods_count, ConstantPool con
   free(method_info);
 }
 
+char *MethodInfo_access_flags_to_string(u2 access_flags)
+{
+  const char *public_string = access_flags & 0x0001 ? "public " : "";
+  const char *private_string = access_flags & 0x0002 ? "private " : "";
+  const char *protected_string = access_flags & 0x0004 ? "protected " : "";
+  const char *static_string = access_flags & 0x0008 ? "static " : "";
+  const char *final_string = access_flags & 0x0010 ? "final " : "";
+  const char *synchronized_string = access_flags & 0x0020 ? "synchronized " : "";
+  const char *bridge_string = access_flags & 0x0040 ? "bridge " : "";
+  const char *varargs_string = access_flags & 0x0080 ? "varargs " : "";
+  const char *native_string = access_flags & 0x0100 ? "native " : "";
+  const char *abstract_string = access_flags & 0x0400 ? "abstract " : "";
+  const char *strict_string = access_flags & 0x0800 ? "strict " : "";
+  const char *synthetic_string = access_flags & 0x1000 ? "synthetic " : "";
+  char *access_flags_string = (char *)malloc(1024);
+  snprintf(
+      access_flags_string, 1024,
+      "%s%s%s%s%s%s%s%s%s%s%s%s",
+      public_string, private_string, protected_string, static_string,
+      final_string, synchronized_string, bridge_string, varargs_string,
+      native_string, abstract_string, strict_string, synthetic_string);
+  if (strlen(access_flags_string) > 0)
+  {
+    access_flags_string[strlen(access_flags_string) - 1] = '\0'; // remove trailing space;
+  }
+  return access_flags_string;
+}
+
 char *MethodInfo_to_string(MethodInfo *method_infos, u2 methods_count, ConstantPool constant_pool)
 {
   char *methods_string = (char *)malloc(65536);
@@ -40,6 +69,8 @@ char *MethodInfo_to_string(MethodInfo *method_infos, u2 methods_count, ConstantP
   for (MethodInfo *method_info = method_infos; method_info < method_infos + methods_count; method_info++)
   {
     char *tmp = (char *)malloc(65536);
+    char *access_flags_string = MethodInfo_access_flags_to_string(
+        method_info->access_flags);
     char *attributes_str = AttributeInfo_to_string(
         method_info->attributes, method_info->attributes_count, constant_pool);
     char comma = method_info == method_infos + methods_count - 1 ? ']' : ',';
@@ -49,12 +80,15 @@ char *MethodInfo_to_string(MethodInfo *method_infos, u2 methods_count, ConstantP
         constant_pool, method_info->descriptor_index);
     snprintf(
         tmp, 65536,
-        "%s{\"access_flags\":\"0x%X\",\"name_index\":\"#%d // %s\","
+        "%s{\"access_flags\":\"0x%04X [%s]\","
+        "\"name_index\":\"#%d // %s\","
         "\"descriptor_index\":\"#%d // %s\", \"attributes_count\":%d,"
         "\"attributes\":%s}%c",
-        methods_string, method_info->access_flags, method_info->name_index,
-        name_utf8, method_info->descriptor_index, descriptor_utf8,
+        methods_string, method_info->access_flags, access_flags_string,
+        method_info->name_index, name_utf8,
+        method_info->descriptor_index, descriptor_utf8,
         method_info->attributes_count, attributes_str, comma);
+    free(access_flags_string);
     free(attributes_str);
     free(name_utf8);
     free(descriptor_utf8);
