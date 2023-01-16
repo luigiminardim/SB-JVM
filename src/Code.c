7a67485530a3ae87b8c75206edf14712ac8c8e6c
Luigi Minardi <luigiminardim@gmail.com>
2023-01-15 23:27:25 -0300

    feat: :construction: Fazer leitor dos 16 primeiros opcodes, que não contém operandos.

--- src/Code.c
+++ src/Code.c
@@ -2,6 +2,32 @@
 #include <stdio.h>  // snprintf
 #include "Code.h"
 
+int Code_is_0_operands(Code code_entry)
+{
+  switch (code_entry.opcode)
+  {
+  case OPCODE_NOP:
+  case OPCODE_ACONST_NULL:
+  case OPCODE_ICONST_M1:
+  case OPCODE_ICONST_0:
+  case OPCODE_ICONST_1:
+  case OPCODE_ICONST_2:
+  case OPCODE_ICONST_3:
+  case OPCODE_ICONST_4:
+  case OPCODE_ICONST_5:
+  case OPCODE_LCONST_0:
+  case OPCODE_LCONST_1:
+  case OPCODE_FCONST_0:
+  case OPCODE_FCONST_1:
+  case OPCODE_FCONST_2:
+  case OPCODE_DCONST_0:
+  case OPCODE_DCONST_1:
+    return 1;
+  default:
+    return 0;
+  }
+}
+
 Code *Code_Parse(u1 *bytes, u4 code_length)
 {
   Code *code = (Code *)malloc(sizeof(Code) * code_length);
@@ -20,11 +46,56 @@ void Code_Free(Code *code, u4 code_length)
 char *Code_entry_to_string(Code code_entry)
 {
   char *final_str = (char *)malloc(2048 * sizeof(char));
-  char *str;
+  const char *str;
   switch (code_entry.opcode)
   {
   case OPCODE_NOP:
     str = "nop";
+    break;
+  case OPCODE_ACONST_NULL:
+    str = "aconst_null";
+    break;
+  case OPCODE_ICONST_M1:
+    str = "iconst_m1";
+    break;
+  case OPCODE_ICONST_0:
+    str = "iconst_0";
+    break;
+  case OPCODE_ICONST_1:
+    str = "iconst_1";
+    break;
+  case OPCODE_ICONST_2:
+    str = "iconst_2";
+    break;
+  case OPCODE_ICONST_3:
+    str = "iconst_3";
+    break;
+  case OPCODE_ICONST_4:
+    str = "iconst_4";
+    break;
+  case OPCODE_ICONST_5:
+    str = "iconst_5";
+    break;
+  case OPCODE_LCONST_0:
+    str = "lconst_0";
+    break;
+  case OPCODE_LCONST_1:
+    str = "lconst_1";
+    break;
+  case OPCODE_FCONST_0:
+    str = "fconst_0";
+    break;
+  case OPCODE_FCONST_1:
+    str = "fconst_1";
+    break;
+  case OPCODE_FCONST_2:
+    str = "fconst_2";
+    break;
+  case OPCODE_DCONST_0:
+    str = "dconst_0";
+    break;
+  case OPCODE_DCONST_1:
+    str = "dconst_1";
     break;
   default:
     snprintf(final_str, 2048, "\"0x%02X\"", code_entry.opcode);
