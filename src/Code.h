337997b639e7141e30d945fa3cdcac3c40b5373c
Luigi Minardi <luigiminardim@gmail.com>
2023-01-17 12:16:03 -0300

    feat: :construction: Read and print from opcode 0x11 to 0x83.

--- src/Code.h
+++ src/Code.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include "java-bytes.h"
+#include "ConstantPool.h"
 
 /**
  * https://en.wikipedia.org/wiki/List_of_Java_bytecode_instructions
@@ -25,17 +26,147 @@ enum Opcode
   OPCODE_DCONST_0 = 0x0E,
   OPCODE_DCONST_1 = 0x0F,
   OPCODE_BIPUSH = 0x10,
+  OPCODE_SIPUSH = 0x11,
+  OPCODE_LDC = 0x12,
+  OPCODE_LDC_W = 0x13,
+  OPCODE_LDC2_W = 0x14,
+  OPCODE_ILOAD = 0x15,
+  OPCODE_LLOAD = 0x16,
+  OPCODE_FLOAD = 0x17,
+  OPCODE_DLOAD = 0x18,
+  OPCODE_ALOAD = 0x19,
+  OPCODE_ILOAD_0 = 0x1A,
+  OPCODE_ILOAD_1 = 0x1B,
+  OPCODE_ILOAD_2 = 0x1C,
+  OPCODE_ILOAD_3 = 0x1D,
+  OPCODE_LLOAD_0 = 0x1E,
+  OPCODE_LLOAD_1 = 0x1F,
+  OPCODE_LLOAD_2 = 0x20,
+  OPCODE_LLOAD_3 = 0x21,
+  OPCODE_FLOAD_0 = 0x22,
+  OPCODE_FLOAD_1 = 0x23,
+  OPCODE_FLOAD_2 = 0x24,
+  OPCODE_FLOAD_3 = 0x25,
+  OPCODE_DLOAD_0 = 0x26,
+  OPCODE_DLOAD_1 = 0x27,
+  OPCODE_DLOAD_2 = 0x28,
+  OPCODE_DLOAD_3 = 0x29,
+  OPCODE_ALOAD_0 = 0x2A,
+  OPCODE_ALOAD_1 = 0x2B,
+  OPCODE_ALOAD_2 = 0x2C,
+  OPCODE_ALOAD_3 = 0x2D,
+  OPCODE_IALOAD = 0x2E,
+  OPCODE_LALOAD = 0x2F,
+  OPCODE_FALOAD = 0x30,
+  OPCODE_DALOAD = 0x31,
+  OPCODE_AALOAD = 0x32,
+  OPCODE_BALOAD = 0x33,
+  OPCODE_CALOAD = 0x34,
+  OPCODE_SALOAD = 0x35,
+  OPCODE_ISTORE = 0x36,
+  OPCODE_LSTORE = 0x37,
+  OPCODE_FSTORE = 0x38,
+  OPCODE_DSTORE = 0x39,
+  OPCODE_ASTORE = 0x3A,
+  OPCODE_ISTORE_0 = 0x3B,
+  OPCODE_ISTORE_1 = 0x3C,
+  OPCODE_ISTORE_2 = 0x3D,
+  OPCODE_ISTORE_3 = 0x3E,
+  OPCODE_LSTORE_0 = 0x3F,
+  OPCODE_LSTORE_1 = 0x40,
+  OPCODE_LSTORE_2 = 0x41,
+  OPCODE_LSTORE_3 = 0x42,
+  OPCODE_FSTORE_0 = 0x43,
+  OPCODE_FSTORE_1 = 0x44,
+  OPCODE_FSTORE_2 = 0x45,
+  OPCODE_FSTORE_3 = 0x46,
+  OPCODE_DSTORE_0 = 0x47,
+  OPCODE_DSTORE_1 = 0x48,
+  OPCODE_DSTORE_2 = 0x49,
+  OPCODE_DSTORE_3 = 0x4A,
+  OPCODE_ASTORE_0 = 0x4B,
+  OPCODE_ASTORE_1 = 0x4C,
+  OPCODE_ASTORE_2 = 0x4D,
+  OPCODE_ASTORE_3 = 0x4E,
+  OPCODE_IASTORE = 0x4F,
+  OPCODE_LASTORE = 0x50,
+  OPCODE_FASTORE = 0x51,
+  OPCODE_DASTORE = 0x52,
+  OPCODE_AASTORE = 0x53,
+  OPCODE_BASTORE = 0x54,
+  OPCODE_CASTORE = 0x55,
+  OPCODE_SASTORE = 0x56,
+  OPCODE_POP = 0x57,
+  OPCODE_POP2 = 0x58,
+  OPCODE_DUP = 0x59,
+  OPCODE_DUP_X1 = 0x5A,
+  OPCODE_DUP_X2 = 0x5B,
+  OPCODE_DUP2 = 0x5C,
+  OPCODE_DUP2_X1 = 0x5D,
+  OPCODE_DUP2_X2 = 0x5E,
+  OPCODE_SWAP = 0x5F,
+  OPCODE_IADD = 0x60,
+  OPCODE_LADD = 0x61,
+  OPCODE_FADD = 0x62,
+  OPCODE_DADD = 0x63,
+  OPCODE_ISUB = 0x64,
+  OPCODE_LSUB = 0x65,
+  OPCODE_FSUB = 0x66,
+  OPCODE_DSUB = 0x67,
+  OPCODE_IMUL = 0x68,
+  OPCODE_LMUL = 0x69,
+  OPCODE_FMUL = 0x6A,
+  OPCODE_DMUL = 0x6B,
+  OPCODE_IDIV = 0x6C,
+  OPCODE_LDIV = 0x6D,
+  OPCODE_FDIV = 0x6E,
+  OPCODE_DDIV = 0x6F,
+  OPCODE_IREM = 0x70,
+  OPCODE_LREM = 0x71,
+  OPCODE_FREM = 0x72,
+  OPCODE_DREM = 0x73,
+  OPCODE_INEG = 0x74,
+  OPCODE_LNEG = 0x75,
+  OPCODE_FNEG = 0x76,
+  OPCODE_DNEG = 0x77,
+  OPCODE_ISHL = 0x78,
+  OPCODE_LSHL = 0x79,
+  OPCODE_ISHR = 0x7A,
+  OPCODE_LSHR = 0x7B,
+  OPCODE_IUSHR = 0x7C,
+  OPCODE_LUSHR = 0x7D,
+  OPCODE_IAND = 0x7E,
+  OPCODE_LAND = 0x7F,
+  OPCODE_IOR = 0x80,
+  OPCODE_LOR = 0x81,
+  OPCODE_IXOR = 0x82,
+  OPCODE_LXOR = 0x83,
 };
+
+typedef struct
+{
+  int8_t byte;
+} _1ByteOperands;
+
+typedef struct
+{
+  int16_t short_;
+} _1ShortOperands;
+
+typedef struct
+{
+  u2 cp_index;
+  int operands_num_bytes;
+} _1CpIndexOperands;
 
 typedef struct
 {
   Opcode opcode;
   union
   {
-    struct
-    {
-      int8_t byte;
-    } _1byte_operands;
+    _1ByteOperands _1byte_operands;
+    _1ShortOperands _1short_operands;
+    _1CpIndexOperands _1cp_index_operands;
   };
 } Code;
 
@@ -43,4 +174,4 @@ Code *Code_Parse(u1 *bytes, u4 code_length);
 
 void Code_Free(Code *code, u4 code_length);
 
-char *Code_to_string(Code *code, u4 code_length);
+char *Code_to_string(Code *code, u4 code_length, ConstantPool constant_pool);
