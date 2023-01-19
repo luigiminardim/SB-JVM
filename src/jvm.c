fb5392c9c9b42fa2ff242da6351a7672b9ef42e8
thiago <thiago.tokarski2001@gmail.com>
2023-01-19 17:09:40 -0300

    feat:sparkles:: Add push e pop frame

--- src/jvm.c
+++ src/jvm.c
@@ -24,3 +24,25 @@ void setMethod(JVM* jvm, char* method_name){
     m = getMethod(jvm->current_class, method_name);
     jvm->current_method = m;
 }
+
+void popFrame(JVM* jvm){
+    // JVM
+    // Frame *f = jvm->frames[jvm->frame_count - 1];
+    // jvm->frame_count = jvm->frame_count - 1;
+    
+    // Frame
+    // freeFrame(f);
+}
+
+void pushFrame(JVM* jvm){
+    ClassFile *new_current_class = jvm->current_class;
+    MethodInfo *new_curret_method = jvm->current_method;
+
+    jvm->frame_count = jvm->frame_count + 1
+    // Frame* new_frame_stack = realloc(jvm->frames, jvm->frame_count);
+    // Frame* new_frame = createframe(jvm, new_current_class, new_current_method);
+    
+    // new_frame_stack[jvm->frame_count-1] = new_frame;
+    // jvm->frames = new_frame_stack;
+    jvm->pc = 0;
+}
