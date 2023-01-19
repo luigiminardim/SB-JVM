e21fb93cc947405431d7ce81ef31615c99fe4bc6
thiago <thiago.tokarski2001@gmail.com>
2023-01-19 15:19:28 -0300

    feat :sparkles: : interface inicial jvm

--- assets/test-Method/SumNumber.java
+++ assets/test-Method/SumNumber.java
@@ -5,10 +5,14 @@ public class SumNumber {
     protected boolean third_attribute = true;
     protected char fourth_attribute = '4';
 
+    static {System.out.println("69");}
+    {System.out.println("96");}
+
     public static void main(String[] args) {
         System.out.println(SumNumber.Soma(3,6));
         SumNumber somaClasse = new SumNumber();
         System.out.println(somaClasse.second_attribute);
+        somaClasse.Subtracao(20, 10);
     }   
 
     public static boolean TestaExcecao(){
@@ -24,6 +28,11 @@ public class SumNumber {
 
     public static int Soma(int a, int b){
         int c = a + b;
+        return c;
+    }
+    
+    public int Subtracao(int a, int b){
+        int c = a - b;
         return c;
     }
 }
---
+++ src/jvm.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "java-bytes.h"
+#include "ClassFile.h"
+#include "MethodArea.h"
+
+typedef struct JVM
+{
+  u4 pc;
+  u2 frame_count; // Também atua como index do frame atual já que se trata de uma pilha
+  // Frame *frames;
+  u2 method_area_count;
+  MethodArea* method_area;
+  MethodArea* current_class;
+  MethodInfo* current_method; // Index para vetor de Methods da Classe atual
+} JVM;
+
+JVM *startJVM();
+
+// Classfile ou Class
+void loadClass(JVM* jvm, char* classname);
+
+void setMethod(JVM* jvm, char* method_name);
+
+void popFrame(JVM*);
+
+void pushFrame(JVM*);
+
+void saveContext(JVM*);
+
+void restoreContext(JVM*);
+
+void runJVM(JVM*);
+
+void freeJVM(JVM* jvm);
